fa6d6428801aa99d85125461230abad86abf7940
pngari09 <pngari09@gmail.com>
2025-04-28 09:44:57 +0300

    initital commit

---
+++ jambo_Kenya.py
@@ -0,0 +1,10 @@
+msg = "jambo Kenya tuko Kapiti plains"
+print(msg)
+
+if msg == "jambo Kenya tuko Kapiti plains":
+
+    print("karibu Kenya kapiti plains")
+
+if msg != "jambo Kenya tuko Kapiti plains":
+
+    print("we are not in kapiti")
